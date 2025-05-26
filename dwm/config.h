fe4f12c33c8875cf24d730daac66f038c644eed2
Mohammad Elahi <moh.elahii@gmail.com>
2025-05-26 15:18:19 -0400

    adding systray support

--- dwm/config.h
+++ dwm/config.h
@@ -3,17 +3,22 @@
 /* appearance */
 static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
+static const unsigned int systrayonleft = 0;    /* 0: systray in the right corner, >0: systray on left of status text */
+static const unsigned int systrayspacing = 2;   /* systray spacing */
+static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
+static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "HackNerdFont:pixelsize=15:antialias=true:autohint=true", "NotoColorEmoji:pixelsize=15:antialias=true:autohint=true", "monospace:size=15" };
 static const char dmenufont[]       = "HackNerdFont:pixelsize=15:antialias=true:autohint=true";
-static char normbgcolor[]           = "#222222";
-static char normbordercolor[]       = "#444444";
-static char normfgcolor[]           = "#bbbbbb";
-static char selfgcolor[]            = "#eeeeee";
-static char selbordercolor[]        = "#770000";
-static char selbgcolor[]            = "#005577";
-static char *colors[][3] = {
+static const char normbgcolor[]           = "#222222";
+static const char normbordercolor[]       = "#444444";
+static const char normfgcolor[]           = "#bbbbbb";
+static const char selfgcolor[]            = "#eeeeee";
+static const char selbordercolor[]        = "#770000";
+static const char selbgcolor[]            = "#005577";
+static const char *colors[][3] = {
        /*               fg           bg           border   */
        [SchemeNorm] = { normfgcolor, normbgcolor, normbordercolor },
        [SchemeSel]  = { selfgcolor,  selbgcolor,  selbordercolor  },
