cf43687757b003a38c5f8055a1d3a4ef5506c420
Mohammad Elahi <moh.elahii@gmail.com>
2025-05-27 08:25:28 -0400

    changing issues with font

--- dmenu/config.h
+++ dmenu/config.h
@@ -4,9 +4,9 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-        "HackNerdFont:pixelsize=15:antialias=true:autohint=true",
-        "NotoColorEmoji:pixelsize=22:antialias=true:autohint=true",
-	"monospace:size=15"
+        "HackNerdFont:pixelsize=18:antialias=true:autohint=true",
+        "NotoColorEmoji:pixelsize=18:antialias=true:autohint=true",
+	"monospace:size=18"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
