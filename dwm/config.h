1fe9c1dd13df804f66f1862df5021a8f6975b71d
Mohammad Elahi <moh.elahii@gmail.com>
2025-08-21 17:33:23 -0400

    setting default shell to zsh for the st and other minor changes in dwm config file

--- dwm/config.h
+++ dwm/config.h
@@ -1,29 +1,31 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static unsigned int borderpx  = 3;        /* border pixel of windows */
-static unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
-static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
-static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
-static const unsigned int systrayonleft = 0;    /* 0: systray in the right corner, >0: systray on left of status text */
-static const unsigned int systrayspacing = 2;   /* systray spacing */
-static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
-static const int showsystray        = 1;        /* 0 means no systray */
-static int showbar            = 1;        /* 0 means no bar */
-static int topbar             = 1;        /* 0 means bottom bar */
-static char font[]            = "HackNerdFont:pixelsize=15:antialias=true:autohint=true";
-static char dmenufont[]       = "HackNerdFont:pixelsize=15:antialias=true:autohint=true";
-static const char *fonts[]          = { font, "NotoColorEmoji:pixelsize=15:antialias=true:autohint=true", "monospace:size=15" };
-static char normbgcolor[]           = "#222222";
-static char normbordercolor[]       = "#444444";
-static char normfgcolor[]           = "#bbbbbb";
-static char selfgcolor[]            = "#eeeeee";
-static char selbordercolor[]        = "#005577";
-static char selbgcolor[]            = "#005577";
+static unsigned int borderpx               = 3;        /* border pixel of windows */
+static unsigned int snap                   = 32;       /* snap pixel */
+static const unsigned int gappih           = 10;       /* horiz inner gap between windows */
+static const unsigned int gappiv           = 10;       /* vert inner gap between windows */
+static const unsigned int gappoh           = 10;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov           = 10;       /* vert outer gap between windows and screen edge */
+static       int smartgaps                 = 0;        /* 1 means no outer gap when there is only one window */
+static const unsigned int systraypinning   = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
+static const unsigned int systrayonleft    = 0;    /* 0: systray in the right corner, >0: systray on left of status text */
+static const unsigned int systrayspacing   = 2;   /* systray spacing */
+static const int systraypinningfailfirst   = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
+static const int showsystray               = 1;        /* 0 means no systray */
+static int showbar                         = 1;        /* 0 means no bar */
+static int topbar                          = 1;        /* 0 means bottom bar */
+
+static char font[]                         = "HackNerdFont:pixelsize=15:antialias=true:autohint=true";
+static char dmenufont[]                    = "HackNerdFont:pixelsize=15:antialias=true:autohint=true";
+static const char *fonts[]                 = { font, "NotoColorEmoji:pixelsize=15:antialias=true:autohint=true", "monospace:size=15" };
+
+static char normbgcolor[]                  = "#222222";
+static char normbordercolor[]              = "#444444";
+static char normfgcolor[]                  = "#bbbbbb";
+static char selfgcolor[]                   = "#eeeeee";
+static char selbordercolor[]               = "#005577";
+static char selbgcolor[]                   = "#005577";
 static char *colors[][3] = {
        /*               fg           bg           border   */
        [SchemeNorm] = { normfgcolor, normbgcolor, normbordercolor },
