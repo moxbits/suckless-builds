124080690b7e7a5158c3c22422ef5c95a7df39e2
Mohammad Elahi <moh.elahii@gmail.com>
2026-01-04 09:48:58 +0100

    feat: integrate slock screen locker and update dwm key bindings
    - Added slock screen locker utility with configurable key binding (Mod+Shift+x)
    - Changed brightness controls from xbacklight to brightnessctl
    - Improved formatting and alignment in dwm config.h

--- dwm/config.h
+++ dwm/config.h
@@ -1,24 +1,24 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static unsigned int borderpx  = 3;        /* border pixel of windows */
-static unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
-static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
-static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
-static const unsigned int systrayonleft = 0;    /* 0: systray in the right corner, >0: systray on left of status text */
-static const unsigned int systrayspacing = 2;   /* systray spacing */
-static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
-static const int showsystray        = 1;        /* 0 means no systray */
-static int showbar            = 1;        /* 0 means no bar */
-static int topbar             = 1;        /* 0 means bottom bar */
+static unsigned int borderpx              = 3;        /* border pixel of windows */
+static unsigned int snap                  = 32;       /* snap pixel */
+static const unsigned int gappih          = 10;       /* horiz inner gap between windows */
+static const unsigned int gappiv          = 10;       /* vert inner gap between windows */
+static const unsigned int gappoh          = 10;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov          = 10;       /* vert outer gap between windows and screen edge */
+static                int smartgaps       = 0;        /* 1 means no outer gap when there is only one window */
+static const unsigned int systraypinning  = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
+static const unsigned int systrayonleft   = 0;    /* 0: systray in the right corner, >0: systray on left of status text */
+static const unsigned int systrayspacing  = 2;   /* systray spacing */
+static const int systraypinningfailfirst  = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
+static const int showsystray              = 1;        /* 0 means no systray */
+static int showbar                        = 1;        /* 0 means no bar */
+static int topbar                         = 1;        /* 0 means bottom bar */
 
-static char font[]                         = "HackNerdFont:pixelsize=15:antialias=true:autohint=true";
-static char dmenufont[]                    = "HackNerdFont:pixelsize=15:antialias=true:autohint=true";
-static const char *fonts[]                 = { font, "NotoColorEmoji:pixelsize=15:antialias=true:autohint=true", "monospace:size=15" };
+static char font[]          = "HackNerdFont:pixelsize=15:antialias=true:autohint=true";
+static char dmenufont[]     = "HackNerdFont:pixelsize=15:antialias=true:autohint=true";
+static const char *fonts[]  = { font, "NotoColorEmoji:pixelsize=15:antialias=true:autohint=true", "monospace:size=15" };
 
 static char normbgcolor[]           = "#222222";
 static char normbordercolor[]       = "#444444";
@@ -27,9 +27,9 @@ static char selfgcolor[]            = "#eeeeee";
 static char selbordercolor[]        = "#005577";
 static char selbgcolor[]            = "#005577";
 static char *colors[][3] = {
-       /*               fg           bg           border   */
-       [SchemeNorm] = { normfgcolor, normbgcolor, normbordercolor },
-       [SchemeSel]  = { selfgcolor,  selbgcolor,  selbordercolor  },
+  /*               fg           bg           border   */
+  [SchemeNorm] = { normfgcolor, normbgcolor, normbordercolor },
+  [SchemeSel]  = { selfgcolor,  selbgcolor,  selbordercolor  },
 };
 
 /* tagging */
@@ -133,12 +133,13 @@ static const Key keys[] = {
   { 0,                            XK_Print,  spawn,          SHCMD("maim ~/pictures/screenshots/screenshot_$(date +%s).png") },
   { ShiftMask,                    XK_Print,  spawn,          SHCMD("maim -s ~/pictures/screenshots/screenshot_$(date +%s).png") },
   { ControlMask,                  XK_Print,  spawn,          SHCMD("maim -s | xclip -sel clip -t image/png") },
-  { 0, XF86XK_MonBrightnessUp,               spawn,          {.v = (const char*[]){"xbacklight", "-inc", "10", NULL}} },
-  { 0, XF86XK_MonBrightnessDown,             spawn,          {.v = (const char*[]){"xbacklight", "-dec", "10", NULL}} },
+  { 0, XF86XK_MonBrightnessUp,               spawn,          {.v = (const char*[]){"brightnessctl", "set", "+5%", NULL}} },
+  { 0, XF86XK_MonBrightnessDown,             spawn,          {.v = (const char*[]){"brightnessctl", "set", "5%-", NULL}} },
   { 0, XF86XK_AudioLowerVolume,              spawn,          {.v = (const char*[]){"wpctl", "set-volume", "@DEFAULT_SINK@", "5%-", NULL}} },
   { 0, XF86XK_AudioRaiseVolume,              spawn,          {.v = (const char*[]){"wpctl", "set-volume", "@DEFAULT_SINK@", "5%+", NULL}} },
   { 0, XF86XK_AudioMute,                     spawn,          {.v = (const char*[]){"wpctl", "set-mute", "@DEFAULT_SINK@", "toggle", NULL}} },
   { 0, XF86XK_AudioMicMute,                  spawn,          {.v = (const char*[]){"wpctl", "set-mute", "@DEFAULT_SOURCE@", "toggle", NULL}} },
+  { MODKEY|ShiftMask,             XK_x,      spawn,          {.v = (const char*[]){"slock", NULL}} },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
